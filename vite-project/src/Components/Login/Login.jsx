6251b574f4200d515570775bdb5053d6bb1d12df
abhaythanak <abhaythanak@gmail.com>
2024-07-12 12:31:06 +0530

    commit checking

--- vite-project/src/Components/Login/Login.jsx
+++ vite-project/src/Components/Login/Login.jsx
@@ -3,6 +3,7 @@ import Header from "../Header/Header";
 export default function Login() {
   return (
     <>
+    <h1>login page</h1>
       <Header />
       <div className="bg-custom-image bg-cover bg-center h-screen">
         <form action="">
